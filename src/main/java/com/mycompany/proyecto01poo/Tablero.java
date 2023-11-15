d17c2c68d3ad55b01f7e04a37db8b7a655a90c5d
Alison <Alison@DESKTOP-HO0UO9A>
2023-11-15 17:00:00 -0600

    modificacion de tablero

--- src/main/java/com/mycompany/proyecto01poo/Tablero.java
+++ src/main/java/com/mycompany/proyecto01poo/Tablero.java
@@ -1,9 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.proyecto01poo;
 
 public class Tablero {
+    private Matriz casillas;
+    private boolean casillaOcupada;
+    private boolean turnoBlanca;
+
+    public Tablero(Matriz casillas, boolean casillaOcupada, boolean turnoBlanca) {
+        this.casillas = casillas;
+        this.casillaOcupada = casillaOcupada;
+        this.turnoBlanca = turnoBlanca;
+    }
+
+    public boolean getCasillaOcupada() {
+        return casillaOcupada;
+    }
+
+    public void setCasillaOcupada(boolean casillaOcupada) {//el parametro deberia ser la casilla?
+        this.casillaOcupada = casillaOcupada;
+    }
+
+    public boolean getTurnoBlanca() {
+        return turnoBlanca;
+    }
+
+    public void setTurnoBlanca(boolean turnoBlanca) {
+        this.turnoBlanca = turnoBlanca;
+    }
+    
     
 }
