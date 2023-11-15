5adb6d5eb53837b2d8b6d91e8382273d1a3a664f
JP113z <josepablof113@gmail.com>
2023-11-15 16:37:14 -0600

    Clase ficha

---
+++ Proyecto1POO/src/main/java/com/mycompany/proyecto1poo/Ficha.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto1poo;
+
+/**
+ *
+ * @author JP113
+ */
+public abstract class Ficha {
+    private boolean disponibilidad;
+    private Ficha fichaActual;
+    private int posicionActual;
+    private int nuevaPosicion;
+    private Tablero tablero;
+
+    // Constructor
+    public Ficha(String nombre, boolean disponibilidad, Ficha actualFicha) {
+        // Implementa tu lógica para inicializar la ficha
+    }
+
+    // Métodos getters y setters
+    public boolean getDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(boolean disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+    public Ficha getActualFicha() {
+        return fichaActual;
+    }
+
+    public void setActualFicha(Ficha fichaActual) {
+        this.fichaActual = fichaActual;
+    }
+
+    // Método abstracto para validar el movimiento, implementado por las subclases
+    public abstract boolean validarMovimiento();
+}
---
+++ Proyecto1POO/src/main/java/com/mycompany/proyecto1poo/Proyecto1POO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto1poo;
+
+/**
+ *
+ * @author JP113
+ */
+public class Proyecto1POO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
